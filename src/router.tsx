8e06037a81733a43f5f8d45c240fdb62edfee4e8
Konstantin <k.podyganov@mail.ru>
2024-04-02 14:48:25 +0300

    Bootstrap project

---
+++ src/router.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import { Root } from "./pages";
+
+export const router = createBrowserRouter([
+  {
+    element: <Root />,
+    children: [],
+  },
+]);
---
+++ src/utils/classes.util.ts
@@ -0,0 +1,5 @@
+export type ClassName = string | number | boolean | null | undefined;
+
+export const classes = (...args: ClassName[]) => {
+  return args.filter(Boolean).join(" ");
+};
